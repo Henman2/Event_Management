9dc9749d9c42e3b1d9bdef491a38ece2a345bd68
Henman2 <47754370+Henman2@users.noreply.github.com>
2022-09-20 04:37:57 +0000

    Initial files added
    Initial files for the project were added

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content=""/>
+    <meta name="keyword" content="Esteemed Evants Management, Events Management in Ghana,
+    Ghana Events Management, top event Management in Ghana, PR and Event Magement"/>
+    <title>Esteemed Events Managment</title>
+
+</head>
+<body>
+    <!-- header -->
+    <header>
+        <div id="logo">
+
+        </div>
+        <nav id="mainMenu">
+          <ul></
+
+        </nav>
+
+    </header>
+
+    <!-- main wrapper -->
+    <main>
+
+    </main>
+
+    <!-- main slides -->
+    <div class="slider">
+
+    </div>
+
+
+
+
+</body>
+</html>
